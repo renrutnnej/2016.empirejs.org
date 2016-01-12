968375afd9e60fe6fd298bb520ea0b9f44ac6c91
indexzero <charlie.robbins@gmail.com>
2016-01-11 22:44:04 -0800

    [dist] Wholesale import of base code for 2015 site.

---
+++ index.js
@@ -0,0 +1,14 @@
+require('babel/register')({
+  ignore:  /node_modules\/(?!paradigm.*)|(paradigm-gulp-watch)/
+});
+
+require('babel/polyfill');
+
+var Server = require('paradigm-server-express')
+
+var server = new Server({
+  paths: { routes: 'routes' },
+  port: 6060
+});
+
+server.start();
