bfe94fc3003ab914613575cf5c4e50fa17b38acc
Chris Abrams <mail@chrisabrams.com>
2016-03-14 11:22:24 -0400

    load partials & helpers

--- server/express.js
+++ server/express.js
@@ -1,6 +1,8 @@
 import bodyParser      from 'body-parser'
 import {chalk, logger} from '../lib/logger'
 import express         from 'express'
+import fs              from 'fs'
+import glob            from 'glob'
 import hbs             from 'hbs'
 import path            from 'path'
 import serveStatic     from 'serve-static'
@@ -9,6 +11,8 @@ function removePoweredBy(req, res, next) {
   res.removeHeader('X-Powered-By')
   next()
 }
+
+var cwd = process.cwd()
 
 class Server {
 
@@ -26,8 +30,49 @@ class Server {
     this.server.set('view engine', 'hbs')
     this.server.engine('hbs', hbs.__express)
 
+    this.loadHelpers()
+    this.loadPartials()
     this.loadRoutes()
 
+  }
+
+  loadHelpers() {
+
+    var helpers = glob.sync('./views/helpers/**/*.js', {cwd})
+    console.error('helpers list', helpers)
+    helpers.forEach( function(helper) {
+      console.error('helper', helper)
+      require(path.join(cwd, helper))(hbs)
+
+    })
+
+  }
+
+  loadPartials() {
+    var partials = glob.sync('./views/partials/**/*.hbs', {cwd})
+
+    partials.forEach( function(partial) {
+
+      var matches = partial.split('.hbs')
+
+      if (!matches) {
+        return
+      }
+
+      var name = matches[0].replace('./views/', '')
+      var template = fs.readFileSync(path.join(cwd, partial), 'utf8')
+
+      hbs.registerPartial(name, template)
+
+    })
+
+    var partialLoaders = glob.sync('../views/partials/**/*.js')
+
+    partialLoaders.forEach( function(partialLoader) {
+
+      require(path.join(cwd, partialLoader))(hbs)
+
+    })
   }
 
   loadRoutes() {
