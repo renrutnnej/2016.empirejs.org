3b480211831c62bafb0f2e2ab98d89f4183222c5
Todd Hamilton <toddkh@gmail.com>
2016-03-15 12:58:39 -0400

    working through rebase

--- styles/base/variables.js
+++ styles/base/variables.js
@@ -3,5 +3,10 @@ module.exports = {
   xs: '320px',
   sm: '768px',
   md: '992px',
-  lg: '1200px'
+  lg: '1200px',
+  empire_green: '#0eaa7f',
+  empire_yellow: '#FFF017',
+  empire_orange: '#f1b034',
+  almostblack: 'rgb(57, 56, 54)',
+  brightblue:  '#51b8f2'
 }
